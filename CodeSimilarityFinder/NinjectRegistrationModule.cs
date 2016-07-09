fa28d1c6b1bc308929784f949311119686209659
Vasyl Korzhyk <k2.v@ukr.net>
2016-07-10 00:06:42 +0300

    Revert "Revert "Created separate class""
    This reverts commit b9c283c67a4af7be10c54101f99e36cfaabd1193.

---
+++ CodeSimilarityFinder/NinjectRegistrationModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Ninject;
+using Ninject.Modules;
+
+namespace CodeSimilarityFinder
+{
+    class NinjectRegistrationModule : INinjectModule
+    {
+        public IKernel Kernel { get; }
+        public void OnLoad(IKernel kernel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnUnload(IKernel kernel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnVerifyRequiredModules()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name { get; }
+    }
+}
